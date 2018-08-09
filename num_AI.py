e92cff8b766496018b6592819e8da03f5a758574
AllanChain <txsmlf@gmail.com>
2018-08-10 07:25:47 +0800

    add new control system

--- num_AI.py
+++ num_AI.py
@@ -2,10 +2,11 @@ import random
 from itertools import combinations
 
 RANGE=list('123456789')
-SEQ=4
-posblties=[]
-guess_in=[]
+rest=SEQ=4
+posblties=set(tuple())
+guess_in=set()
 guess_pos=[]
+history={}
 cnum=random.sample(RANGE,k=SEQ)
 def judge(c,u):
     a=b=0
@@ -25,31 +26,74 @@ def contain(a,b):
     elif b_set in a_set:
         return (b,a)
     else:
-        return False
-def find_same(la,lb):
+        return None,None
+def merge(la,lb,subs):
+    #if not la:return lb
+    #if not lb:return la
+    posblty=set()
+    for i in la:
+        for j in lb:
+            p=tuple(set(i)|set(j))
+            if not p in subs and len(p)<=rest:
+                posblty.add(p)
+    return posblty
+def clear(l):
+    l=list(l)
+    removes=set()
+    for i in range(len(l)):
+        for j in range(i+1,len(l)):
+            p,c=contain(l[i],l[j])
+            if not p is None:
+                removes.add(p)
+    return set(l)-removes
+def history_clear(l):
+    for posblty in l.copy():
+        for h,r in history.items():
+            if len(set(posblty)&set(h))>r[1]:
+                l.remove(posblty)
+    return l
+def calc_posblty(la,lb):
     same=set()
-    subs=[]
-    exclude=[]
+    subs=set()
+    exclude=set()
     for i in la:
         for j in lb:
             if i==j:
                 same.add(i)
-            elif j in i:
-                if j in subs:
-                    exclude.append(j)
-                    subs.remove(j)
-                subs.append(j)
-            elif i in j:
-                if i in subs:
-                    exclude.append(i)
-                    subs.remove(i)
-                subs.append(j)
-    la=la.difference(same)
-    lb=lb.difference(same)
-    return same,la,lb
+            else:
+                p,c=contain(j,i)
+                if p is None:
+                    continue
+                if p in subs:
+                    exclude.add(i)
+                else:
+                    subs.add(p)
+    la=la-same-subs
+    lb=lb-same-subs
+    subs-=exclude
+    #print(same,subs,la,lb,merge(la,lb,subs))
+    return clear(same|subs|merge(la,lb,subs))
 def guess(num):
-    a,b=judge(num)
-    my_posblty=list(combinations(num,a+b))
-    for posblty in posblties:
-        same,lo,lm=find_same(my_posblty,posblty)
-
+    global posblties
+    a,b=judge(cnum,num)
+    history[''.join(num)]=(a,b+a)
+    if a+b==rest:
+        return False
+    my_posblty=set(combinations(num,a+b))
+    posblties=calc_posblty(my_posblty,posblties) if posblties else my_posblty
+    posblties=history_clear(posblties)
+    print(posblties)
+    list_posblyties=list(posblties)
+    n=random.choice(list_posblyties)
+    while len(n)<4:
+        n=tuple(set(n)|set(random.choice(list_posblyties)))
+    return n[:4]
+n=guess(list(str(1234)))
+n=guess(list(str(5678)))
+rest=history['1234'][1]+history['5678'][1]
+print(rest)
+for i in range(5):
+    n=guess(n)
+    if n==False:
+        break
+print(history)
